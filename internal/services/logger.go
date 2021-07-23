7c1fc9e174d8b5874f6cdc2e5170aa029b9ab167
Niroop Maneti <Niroop.Maneti@carrier.com>
2021-07-23 18:56:54 +0530

    restructuring the repo once again

--- internal/services/logger.go
+++ internal/services/logger.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/niroopreddym/go-llimitedfilesizelog/src/internal/enums"
+	"github.com/niroopreddym/go-llimitedfilesizelog/internal/enums"
 )
 
 //LoggerService implementsthe Ilogger.go service
